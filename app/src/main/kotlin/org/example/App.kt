68b93b9e7c8a6e611f02f8fc79744e6870ca54d5
abril-mayo <abrilmeono@gmail.com>
2026-05-11 09:06:12 -0300

    kotlin

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -3,18 +3,38 @@
  */
 package org.example
 
-class Complejo {
-    private val real: Int
-    private val imaginario: Int
-
-    fun inicializar(real: Int, imaginario: Int){
+class Complejo(private var real: Int=0, private var imag: Int=0){
+    fun inicializar(real:Int,imag:Int){
         this.real=real
-        this.real=real
+        this.imag=imag
+    }
+    fun mostrar(){
+        println(this.toString())
+    }
+    override fun toString():String{
+        return "(${real},${imag})"
+    }
+    fun sumar(otro: Complejo): Complejo {
+        return Complejo(
+            real + otro.real,
+            imag + otro.imag
+        )
+    }
+    fun restar(otro: Complejo): Complejo {
+        return Complejo(
+            real - otro.real,
+            imag - otro.imag
+        )
     }
 }
 
 fun main() {
-    val c = Complejo(3.0, 4.0)
-    println(c.mostrar())
-    pirntln("hola")
+    var complejo: Complejo
+    complejo= Complejo()
+    complejo.inicializar(3,4)
+    println("mi número complejo es ${complejo.toString()}")
+    var c2:Complejo= Complejo(1,2)
+    var c3: Complejo
+    c3=complejo.sumar(c2)
+    c3.mostrar()
 }
